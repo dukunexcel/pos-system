9041c3aff690915148bfbde36cacdb2db9b64ab5
dukun excel <dukunexcel@gmail.com>
2026-08-23 09:33:11 +0700

    Upload API Login Pertama

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Selamat Datang di Sistem POS</h1>
+      <p>Aplikasi ini akan menjadi sistem kasir sederhana.</p>
+      
+      <div>
+        <h2>Fitur yang Akan Dibuat:</h2>
+        <ul>
+          <li>Manajemen Produk</li>
+          <li>Transaksi Penjualan</li>
+          <li>Laporan Penjualan</li>
+          <li>Manajemen User</li>
+        </ul>
+      </div>
+    </main>
+  )
+}
